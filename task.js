2ecb11c2c1623c4219d0f6e4352a7b3955135792
Anna Kazmirchuk <93005766+AnnaKazmirchuk@users.noreply.github.com>
2022-09-24 16:43:20 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.dropdown');
+const list = document.querySelector('.dropdown__list');
+const link = document.querySelectorAll('.dropdown__link');
+const value = document.querySelector('.dropdown__value');
+
+menu.addEventListener('click', function(event) {
+    let target = event.target;
+
+    if (target.classList.contains('dropdown__value')) {
+        list.classList.add('dropdown__list_active')
+    }
+
+    if (target.classList.contains('dropdown__link')) {
+        value.textContent = target.textContent;
+        list.classList.remove('dropdown__list_active')
+    }
+
+    event.preventDefault();
+})
